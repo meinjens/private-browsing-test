a8b1a4f1fea7d80d830852b00771ed06460f9ffd
Jens Meinecke <Jens.Meinecke@agon.solutions.de>
2016-01-29 13:42:51 +0100

    initialize customstorage

--- js/custom-storage.js
+++ js/custom-storage.js
@@ -2,44 +2,32 @@
 	
 	'use strict';
 	
-	window.customStorage = {};
-	
-	
+	window.customStorage = function() {
+		return {
+			set: function(key, value) {
+	            storage.setItem(key, value);
+	        },
+	        get: function(key, defaultValue) {
+	            return storage.getItem(key) || defaultValue;
+	        },
+	        setObject: function(key, value) {
+	            storage.setItem(key, JSON.stringify(value));
+	        },
+	        getObject: function(key) {
+	            return JSON.parse(storage.getItem(key) || '{}');
+	        },
+	        remove: function(key){
+	            storage.removeItem(key);
+	        },
+	        clear: function() {
+	            storage.clear();
+	        },
+	        key: function(index){
+	            storage.key(index);
+	        }
+		};	
+	};
 	
 	console.log(window.customStorage);
 	
-	
 }());
-
-
-
-
-var StorageFactory = function() {
-	
-	var storage = new CustomStorage();
-	
-	return {
-		set: function(key, value) {
-            storage.setItem(key, value);
-        },
-        get: function(key, defaultValue) {
-            return storage.getItem(key) || defaultValue;
-        },
-        setObject: function(key, value) {
-            storage.setItem(key, JSON.stringify(value));
-        },
-        getObject: function(key) {
-            return JSON.parse(storage.getItem(key) || '{}');
-        },
-        remove: function(key){
-            storage.removeItem(key);
-        },
-        clear: function() {
-            storage.clear();
-        },
-        key: function(index){
-            storage.key(index);
-        }
-	}
-	
-}
